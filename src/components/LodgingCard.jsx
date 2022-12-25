3218a301d3b3097765b01f9cebdb31370fed6892
Fabien Dev <fabiendevfront@gmail.com>
2022-12-25 23:38:57 +0100

    Code refactoring

--- src/components/LodgingCard.jsx
+++ src/components/LodgingCard.jsx
@@ -10,13 +10,12 @@ import { Link } from "react-router-dom";
 const LodgingCard = ({ id, cover, title }) => {
     return (
         <div className="lodging-card">
-            <Link key={id} to={"/lodging/" + id} className="lodging-card__link">
+            <Link to={"/lodging/" + id} className="lodging-card__link">
                 <div className="lodging-card__overlay"></div>
                 <img src={cover} alt="Preview du logement" className="lodging-card__cover" />
                 <h3 className="lodging-card__title">{title}</h3>
             </Link>
         </div>
-
     );
 };
 
